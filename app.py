d1f8a1fc8ea4a816a5ba63fa1cc0cb8effc91e7a
JonasCanesso <akatsumo@hotmail.com>
2024-10-11 10:39:25 -0300

    update
    adicionando pagamento em pix com qr_code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
-
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -9,10 +9,23 @@ app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
 
-
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
-    return jsonify({"message": "Opção de pagamento criado."})
+    data = request.get_json()
+
+    # validações
+    if 'value' not in data:
+        return jsonify({"message": "Valor inválido."})
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+    
+    return jsonify({"message": "Opção de pagamento criado.",
+                   "payment": new_payment.to_dict()})
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
